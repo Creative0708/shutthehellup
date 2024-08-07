a53a338132290b1a91e7c1517c319c95b82725af
Colin Cai <therealcreative0708@gmail.com>
2024-08-07 19:55:11 -0400

    packet ignoring logic yippee (+ README.md)

---
+++ README.md
@@ -0,0 +1,22 @@
+# Shut the Hell Up!
+
+_A Minecraft mod for bodging away packet-related disconnects_
+
+Exactly what it says on the tin. This inserts a try-catch in `net.minecraft.network.Connection` that ignores any
+exceptions thrown due to packet decoding errors. This will prevent players from being disconnected when packet-handling
+code throws an error.
+
+This was made for a private playthrough where players were being disconnected for a sound packet issue:
+
+```
+Can't find id for 'net.minecraft.sounds.SoundEvent@xxxxxxxx' in map Registry[ResourceKey[minecraft:root / minecraft:sound_event] (Experimental)]
+```
+
+I cannot stress enough how much of a bad idea it is to include this mod in your modpack. If you have the time, find the
+root cause and report it to the respective mod authors. Sweeping errors under the rug like this
+is a great way to destabilize your modpack.
+
+That said, if binary searching your modlist takes too long or if you just can't be bothered to do it the right way, this
+mod might be for you.
+
+Good luck and godspeed.
--- src/main/java/io/github/creative0708/shutthehellup/mixin/ConnectionMixin.java
+++ src/main/java/io/github/creative0708/shutthehellup/mixin/ConnectionMixin.java
@@ -1,4 +1,51 @@
 package io.github.creative0708.shutthehellup.mixin;
 
-public class ConnectionMixin {
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.DefaultChannelPromise;
+import net.minecraft.network.Connection;
+import net.minecraft.network.PacketListener;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.server.RunningOnDifferentThreadException;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.concurrent.RejectedExecutionException;
+
+@Mixin(Connection.class)
+public abstract class ConnectionMixin {
+    @Shadow
+    protected static <T extends PacketListener> void genericsFtw(Packet<T> p_129518_, PacketListener p_129519_) {
+    }
+
+    @Shadow
+    @Final
+    private static Logger LOGGER;
+
+    @Redirect(method = "Lnet/minecraft/network/Connection;doSendPacket(Lnet/minecraft/network/protocol/Packet;Lnet/minecraft/network/PacketSendListener;Lnet/minecraft/network/ConnectionProtocol;Lnet/minecraft/network/ConnectionProtocol;)V", at = @At(value = "INVOKE", target = "Lio/netty/channel/Channel;writeAndFlush(Ljava/lang/Object;)Lio/netty/channel/ChannelFuture;"))
+    private ChannelFuture wrapWithTryCatch(Channel instance, Object o) {
+        try {
+            return instance.writeAndFlush(o);
+        } catch (Exception e) {
+            return new DefaultChannelPromise(instance);
+        }
+    }
+
+    @Redirect(method = "Lnet/minecraft/network/Connection;channelRead0(Lio/netty/channel/ChannelHandlerContext;Lnet/minecraft/network/protocol/Packet;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/Connection;genericsFtw(Lnet/minecraft/network/protocol/Packet;Lnet/minecraft/network/PacketListener;)V"))
+    private void wrapWithTryCatch(Packet<?> packet, PacketListener listener) {
+        try {
+            genericsFtw(packet, listener);
+        } catch (RunningOnDifferentThreadException | RejectedExecutionException e) {
+            // rethrow valid exceptions to not prevent e.g. server disconnecting
+            throw e;
+        } catch (Exception e) {
+            LOGGER.error("Received {} that couldn't be processed", packet.getClass(), e);
+        }
+    }
+
 }
